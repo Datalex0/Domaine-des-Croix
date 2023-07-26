d055baf1ecf0ef52e9c367027a1409fdaf9cde55
Alexis <126944670+Datalex0@users.noreply.github.com>
2023-07-26 14:17:22 +0200

    Update 7- Perspectives d'évolution.py

--- SRC/pages/7- Perspectives d'évolution.py
+++ SRC/pages/7- Perspectives d'évolution.py
@@ -16,8 +16,26 @@ st.set_page_config(page_title="Perspectives",
 # Titre centré
 st.markdown(f"<h1 style='text-align: center;'>Perspectives d'évolution</h1>", unsafe_allow_html=True)
 
-st.markdown(" * Ajouter des filtres à la carte pour d'autres comparaisons,")
-st.markdown(" * Traduire les titres de colonnes et les données en français ")
-st.markdown(" * 2 ")
-st.markdown(" * 2 ")
-st.markdown(" * 2 ")
+
+st.markdown(f"""
+<div style="background-color: rgba(400, 400, 400, 0.7); padding: 10px;">
+    <h3 style="font-size: 24px; color: #8F260F;">Perspectives d'Evolution :</h3>
+</div>
+        <br>
+    <ul>
+    <div style="background-color: rgba(400, 400, 400, 0.7); padding: 10px;">
+    <p style="color: #8F260F;"><b>
+         - Ajouter des filtres et des informations à la carte pour d'autres comparaisons,
+        <br>
+        <br>
+         - Traduire les titres de colonnes et les données en français pour les rendre plus lisibles,
+        <br>
+        <br>
+         - Pousser l'exploration, notamment via les colonnes 'région' pour prioriser la meilleure région dans laquelle démarrer l'implantation,
+        <br>
+        <br>
+         - Améliorer l'efficacité de l'algorithme de prédiction de prix,
+        <br>
+        <br>
+         - Retravailler certains Graphiques pour les rendre plus agréables à regarder.
+    """, unsafe_allow_html=True)
